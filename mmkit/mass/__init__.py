91ae67d06579994b1cccb6055b5a11855a2d4acf
MotohiroOGAWA <s213044y@st.go.tuat.ac.jp>
2025-10-20 04:10:37 +0000

    feat: Add MassError and related error classes to module exports

--- mmkit/mass/__init__.py
+++ mmkit/mass/__init__.py
@@ -1,7 +1,11 @@
 from .Adduct import Adduct
 from .AdductIon import AdductIon
+from .MassError import MassError, DaError, PpmError
 
 __all__ = [
     "Adduct",
     "AdductIon",
+    "MassError",
+    "DaError",
+    "PpmError",
 ]
